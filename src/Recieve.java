bee7863bb25cb971cd38f186aecd4aa460d91c6f
raja-ravi-prakash <iamraviprakash1@gmail.com>
2020-03-21 16:03:00 +0530

    .class

--- src/Recieve.java
+++ src/Recieve.java
@@ -1,29 +1,31 @@
 import java.net.*;
 import java.io.*;
 
-public class Recieve extends Thread
-{
+public class Recieve extends Thread {
 	ServerSocket ss;
 	Socket s;
 	DataInputStream in;
-	Recieve(int port)throws Exception{
-		ss=new ServerSocket(port);
-	}
-	public void run(){
-		try{
-			s=ss.accept();
-			
-			in=new DataInputStream(s.getInputStream());
 
-			String msg="";
-			while(!msg.equals("Over")){
-				msg=in.readUTF();
-				System.out.println("\nUnknown : "+msg);
+	Recieve(int port) throws Exception {
+		ss = new ServerSocket(port);
+	}
+
+	public void run() {
+		try {
+			s = ss.accept();
+
+			in = new DataInputStream(s.getInputStream());
+
+			String msg = "";
+			while (!msg.equals("Over")) {
+				msg = in.readUTF();
+				System.out.println("\nUnknown : " + msg);
+				System.out.print("You: ");
 			}
 			ss.close();
 			s.close();
 			in.close();
+		} catch (Exception ae) {
 		}
-		catch(Exception ae){}
 	}
 }
